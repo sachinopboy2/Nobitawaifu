fb0442808f2cb0b55dbba3cfcb29a91c2fff4d2a
Nand Yaduwanshi <gccboys887@gmail.com>
2025-04-02 10:24:14 +0530

    𝐍𝐨𝐱𝐱 𝐍𝐞𝐭𝐰𝐨𝐫𝐤

--- NoxxNetwork/config.py
+++ NoxxNetwork/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6584789596"
-    sudo_users = "6101457748", "5932230962", "6100011620", "6154972031", "6584789596", "5702598840"
-    GROUP_ID = -1002133191051
-    TOKEN = "6462620320:AAGIBSjD_PmyiSNPmF3kdYsO0G2TSMpQe9M"
-    mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "1786683163"
+    sudo_users = "7990474206", "7282752816"
+    GROUP_ID = -1002201996549
+    TOKEN = "7647477376:AAEsHX0rQf1NAT6BdlrreCQ85vLrorkQcyY"
+    mongo_url = "mongodb+srv://kumarN:kumarN@kumarn.vxdui.mongodb.net/?retryWrites=true&w=majority&appName=kumarN"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
-    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
-    UPDATE_CHAT = "Grabbing_Your_WH_Channel"
-    BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
-    CHARA_CHANNEL_ID = "-1002009998662"
+    SUPPORT_CHAT = "NYCREATION_CHATZONE"
+    UPDATE_CHAT = "NOXXNETWORK"
+    BOT_USERNAME = "AmritaxBot"
+    CHARA_CHANNEL_ID = "-1002204134287"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
