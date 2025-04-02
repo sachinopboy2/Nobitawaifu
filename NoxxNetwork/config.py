1904e92a971185a17e7a2deffc5677caef81a0ef
Nand Yaduwanshi <gccboys887@gmail.com>
2025-04-02 10:32:56 +0530

    𝐍𝐨𝐱𝐱 𝐍𝐞𝐭𝐰𝐨𝐫𝐤

--- NoxxNetwork/config.py
+++ NoxxNetwork/config.py
@@ -1,21 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "1786683163"
-    sudo_users = "7990474206", "7282752816"
-    GROUP_ID = -1002201996549
-    TOKEN = "7647477376:AAEsHX0rQf1NAT6BdlrreCQ85vLrorkQcyY"
-    mongo_url = "mongodb+srv://kumarN:kumarN@kumarn.vxdui.mongodb.net/?retryWrites=true&w=majority&appName=kumarN"
-    PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
-    SUPPORT_CHAT = "NYCREATION_CHATZONE"
-    UPDATE_CHAT = "NOXXNETWORK"
-    BOT_USERNAME = "AmritaxBot"
-    CHARA_CHANNEL_ID = "-1002204134287"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = os.getenv("OWNER_ID")
+    sudo_users = os.getenv("SUDO_USERS").split(",")  # Convert CSV to list
+    GROUP_ID = int(os.getenv("GROUP_ID"))
+    TOKEN = os.getenv("TOKEN")
+    mongo_url = os.getenv("MONGO_URL")
+    PHOTO_URL = os.getenv("PHOTO_URL").split(",")  # Convert CSV to list
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
+    UPDATE_CHAT = os.getenv("UPDATE_CHAT")
+    BOT_USERNAME = os.getenv("BOT_USERNAME")
+    CHARA_CHANNEL_ID = int(os.getenv("CHARA_CHANNEL_ID"))
+    api_id = int(os.getenv("API_ID"))
+    api_hash = os.getenv("API_HASH")
 
-    
+
 class Production(Config):
     LOGGER = True
 
